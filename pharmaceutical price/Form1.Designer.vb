93f6c8a398132e3abf0ed1b6fb651685da982a68
Ehabmagdyabbas <ihabmajdyabass@hotmail.com>
2018-08-09 18:16:03 +0200

    Add files via upload

---
+++ pharmaceutical price/Form1.Designer.vb
@@ -0,0 +1,320 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(790, 384)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(169, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "اضافة"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column5, Me.Column3, Me.Column4, Me.Column6})
+        Me.DataGridView1.Location = New System.Drawing.Point(7, 11)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(952, 347)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "اسم الصنف"
+        Me.Column1.Name = "Column1"
+        Me.Column1.Width = 370
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "تكلفة الصنف"
+        Me.Column2.Name = "Column2"
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "الربح فى القطعة"
+        Me.Column5.Name = "Column5"
+        Me.Column5.Width = 130
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "العدد"
+        Me.Column3.Name = "Column3"
+        Me.Column3.Width = 50
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "اجمالى التكلفة"
+        Me.Column4.Name = "Column4"
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "اجمالى العوائد"
+        Me.Column6.Name = "Column6"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(624, 387)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(111, 20)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "test"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(513, 387)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(79, 20)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "0"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(325, 387)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(71, 20)
+        Me.TextBox3.TabIndex = 4
+        Me.TextBox3.Text = "0"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(648, 371)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "اسم الصنف"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(519, 371)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "تكلفة الصنف"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(345, 371)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "العدد"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(89, 433)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(13, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "0"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(415, 371)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(81, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "الربح فى القطعة"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(422, 387)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(71, 20)
+        Me.TextBox4.TabIndex = 11
+        Me.TextBox4.Text = "0"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(364, 433)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(13, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "0"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(334, 417)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(105, 13)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "اجمالى مجموع الارباح"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(63, 417)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(105, 13)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "اجمالى مجموع التكلفة"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(10, 471)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(84, 21)
+        Me.Button3.TabIndex = 16
+        Me.Button3.Text = "load"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(109, 465)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(483, 20)
+        Me.TextBox5.TabIndex = 17
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(10, 449)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(84, 21)
+        Me.Button4.TabIndex = 18
+        Me.Button4.Text = "Save"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(704, 417)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(62, 13)
+        Me.Label9.TabIndex = 20
+        Me.Label9.Text = "عدد الاصناف"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(734, 433)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(13, 13)
+        Me.Label10.TabIndex = 19
+        Me.Label10.Text = "0"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(811, 465)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(134, 24)
+        Me.Button2.TabIndex = 21
+        Me.Button2.Text = "اعادة الحساب"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(971, 497)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Calu Total pharmaceutical price"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Column1 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ pharmaceutical price/Form1.vb
@@ -0,0 +1,163 @@
+﻿Imports System.Threading
+
+Public Class Form1
+
+    Dim yu, uy
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        DataGridView1.Rows.Add({TextBox1.Text, TextBox2.Text, TextBox4.Text, TextBox3.Text})
+        For i = 0 To DataGridView1.RowCount - 1
+            Dim gh, hg, hh As New Double
+            gh = DataGridView1.Rows(i).Cells(1).Value
+            hg = DataGridView1.Rows(i).Cells(2).Value
+            hh = DataGridView1.Rows(i).Cells(3).Value
+
+            DataGridView1.Rows(i).Cells(5).Value = hh * hg
+            DataGridView1.Rows(i).Cells(4).Value = (gh + hg) * DataGridView1.Rows(i).Cells(3).Value
+        Next
+        Dim ro As DataGridViewRow
+        Dim n1 As Integer = 0
+        For Each ro In DataGridView1.Rows
+
+            DataGridView1.Rows(n1).HeaderCell.Value = (1 + n1).ToString
+            DataGridView1.RowHeadersWidth = 80
+            n1 += 1
+        Next
+        uy = 0
+        For y = 0 To DataGridView1.RowCount - 1
+
+            yu = yu + DataGridView1.Rows(y).Cells(4).Value
+            uy = uy + DataGridView1.Rows(y).Cells(5).Value
+            Label4.Text = yu
+
+            Label6.Text = uy
+        Next
+    End Sub
+    Function GetUserName() As String
+        If TypeOf My.User.CurrentPrincipal Is  _
+        Security.Principal.WindowsPrincipal Then
+            ' The application is using Windows authentication.
+            ' The name format is DOMAIN\USERNAME.
+            Dim parts() As String = Split(My.User.Name, "\")
+            Dim username As String = parts(1)
+            Return username
+        Else
+            ' The application is using custom authentication.
+            Return My.User.Name
+        End If
+    End Function
+
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs)
+        For m = 0 To 1000
+            DataGridView1.Rows.Add({TextBox1.Text, TextBox2.Text, TextBox3.Text})
+            For i = 0 To DataGridView1.RowCount - 1
+                DataGridView1.Rows(i).Cells(3).Value = DataGridView1.Rows(i).Cells(1).Value * DataGridView1.Rows(i).Cells(2).Value
+            Next
+            Dim ro As DataGridViewRow
+            Dim n1 As Integer = 0
+            For Each ro In DataGridView1.Rows
+
+                DataGridView1.Rows(n1).HeaderCell.Value = (1 + n1).ToString
+                DataGridView1.RowHeadersWidth = 80
+                n1 += 1
+            Next
+        Next
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        'TextBox5.Text = "C:\Users\" + GetUserName() + "\Desktop\default.txt"
+        TextBox5.Text = Application.StartupPath & "\default.txt"
+    End Sub
+
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        SaveGridData(DataGridView1, TextBox5.Text)
+
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        Label4.Text = 0
+
+        Label6.Text = 0
+        yu = 0
+        uy = 0
+        LoadGridData(DataGridView1, TextBox5.Text)
+        Thread.Sleep(1000)
+        For i = 0 To DataGridView1.RowCount - 1
+            Dim gh, hg, hh As New Double
+            gh = DataGridView1.Rows(i).Cells(1).Value
+            hg = DataGridView1.Rows(i).Cells(2).Value
+            hh = DataGridView1.Rows(i).Cells(3).Value
+
+            DataGridView1.Rows(i).Cells(5).Value = hh * hg
+            DataGridView1.Rows(i).Cells(4).Value = (gh + hg) * DataGridView1.Rows(i).Cells(3).Value
+            Label10.Text = i
+        Next
+        For y = 0 To DataGridView1.RowCount - 1
+
+            yu = yu + DataGridView1.Rows(y).Cells(4).Value
+            uy = uy + DataGridView1.Rows(y).Cells(5).Value
+            Label4.Text = yu
+
+            Label6.Text = uy
+        Next
+        Dim ro As DataGridViewRow
+        Dim n1 As Integer = 0
+        For Each ro In DataGridView1.Rows
+
+            DataGridView1.Rows(n1).HeaderCell.Value = (1 + n1).ToString
+            DataGridView1.RowHeadersWidth = 80
+            n1 += 1
+        Next
+    End Sub
+
+
+
+
+    Private Sub SaveGridData(ByRef ThisGrid As DataGridView, ByVal Filename As String)
+
+        ThisGrid.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableWithoutHeaderText
+
+        ThisGrid.SelectAll()
+
+        IO.File.WriteAllText(Filename, ThisGrid.GetClipboardContent().GetText.TrimEnd)
+
+        ThisGrid.ClearSelection()
+
+    End Sub
+    Private Sub LoadGridData(ByRef ThisGrid As DataGridView, ByVal Filename As String)
+        ThisGrid.Rows.Clear()
+
+
+        For Each THisLine In My.Computer.FileSystem.ReadAllText(Filename).Split(Environment.NewLine)
+
+            ThisGrid.Rows.Add(Split(THisLine, "	"))
+
+        Next
+
+    End Sub
+
+    Private Sub Button2_Click_1(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Label4.Text = 0
+
+        Label6.Text = 0
+        yu = 0
+        uy = 0
+        For y = 0 To DataGridView1.RowCount - 1
+
+            yu = yu + DataGridView1.Rows(y).Cells(4).Value
+            uy = uy + DataGridView1.Rows(y).Cells(5).Value
+            Label4.Text = yu
+
+            Label6.Text = uy
+        Next
+        Dim ro As DataGridViewRow
+        Dim n1 As Integer = 0
+        For Each ro In DataGridView1.Rows
+
+            DataGridView1.Rows(n1).HeaderCell.Value = (1 + n1).ToString
+            DataGridView1.RowHeadersWidth = 80
+            n1 += 1
+        Next
+    End Sub
+End Class
